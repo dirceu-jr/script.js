0d1ae9396e2720bd28fd1179fbd5a599b77fbf9e
Dirceu Jr <dirceuu@gmail.com>
2024-04-03 15:59:47 -0300

    add my current preferred xhr code

--- script.js
+++ script.js
@@ -2,6 +2,28 @@
 
 var App = (function() {
 
+  // XMLHttpRequest for JSON responses
+  var jsonXHR = function(url, method, data, successHandler, errorHandler) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    // mind file uploads
+    if (!(data instanceof FormData)) {
+      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    }
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        var status = xhr.status;
+        if (status == 200) {
+          var data = JSON.parse(xhr.responseText);
+          successHandler && successHandler(data);
+        } else {
+          errorHandler && errorHandler(status);
+        }
+      }
+    }
+    xhr.send(data);
+  }
+
   function private() {
     console.log("=:)");
   }
